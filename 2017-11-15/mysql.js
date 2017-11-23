111780dcfa38fcd0e5cf1faaa6359f5ce9743209
lianlianlu <981696258@qq.com>
2017-11-23 18:08:07 +0800

    mysql实例
    1115课程补充

---
+++ 2017-11-15/mysql.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+
+//1.链接，链接池
+let db = mysql.createPool({
+	host:"localhost",
+	user:"root",
+	password:'',
+	database:"bluetest13"
+
+});
+
+//2.查询
+db.query('select * from user_table',(err,data) => {
+	if(err){
+		console.log(err);
+	}else{
+		console.log(data);
+	}
+});
+
